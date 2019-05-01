b7f80ea06a29c4bd6c68617c29e17819ea8362fa
Cezar Sampaio <cezarsmpio@gmail.com>
2019-05-01 02:20:53 +0200

    First commit :party:

--- README.md
+++ README.md
@@ -1,16 +1,51 @@
 # react-fake-content
 
-[![Travis][build-badge]][build]
-[![npm package][npm-badge]][npm]
-[![Coveralls][coveralls-badge]][coveralls]
+[![https://nodei.co/npm/react-fake-content.png?downloads=true&downloadRank=true&stars=true](https://nodei.co/npm/react-fake-content.png?downloads=true&downloadRank=true&stars=true)](https://www.npmjs.com/package/react-fake-content)
 
-Describe react-fake-content here.
+<!-- badge -->
 
-[build-badge]: https://img.shields.io/travis/user/repo/master.png?style=flat-square
-[build]: https://travis-ci.org/user/repo
+[![minified + gzip](https://badgen.net/bundlephobia/minzip/react-fake-content)](https://bundlephobia.com/result?p=react-fake-content)
+[![npm version](https://img.shields.io/npm/v/react-fake-content.svg)](https://www.npmjs.com/package/react-fake-content)
+[![npm download](https://img.shields.io/npm/dm/react-fake-content.svg)](https://www.npmjs.com/package/react-fake-content)
+[![GitHub stars](https://img.shields.io/github/stars/cezarlz/react-fake-content.svg?style=social&label=Star)](https://github.com/cezarlz/react-fake-content)
+[![GitHub issues](https://img.shields.io/github/issues/cezarlz/react-fake-content.svg)](https://github.com/cezarlz/react-fake-content/issues)
+[![GitHub contributors](https://img.shields.io/github/contributors/cezarlz/react-fake-content.svg)](https://GitHub.com/cezarlz/react-fake-content/graphs/contributors/)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
 
-[npm-badge]: https://img.shields.io/npm/v/npm-package.png?style=flat-square
-[npm]: https://www.npmjs.org/package/npm-package
+## Install
 
-[coveralls-badge]: https://img.shields.io/coveralls/user/repo/master.png?style=flat-square
-[coveralls]: https://coveralls.io/github/user/repo
+```
+npm install react-fake-content --save
+```
+
+## Usage
+
+```js
+import { Circle } from 'react-fake-content';
+
+function Avatar(props) {
+    if (!props.loaded) {
+        return <Circle size="40px" />
+    }
+
+    return <img src={props.url} width="40" height="40">
+}
+```
+
+## Components
+
+### Rectangle
+
+Coming soon.
+
+### Circle
+
+Coming soon.
+
+### Line
+
+Coming soon.
+
+### Square
+
+Coming soon.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,99 @@
-import React, {Component} from 'react'
+import React, { useLayoutEffect } from 'react';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+function useInjectStyle(rule) {
+  useLayoutEffect(function() {
+    try {
+      const styleElement = document.createElement('style');
+      styleElement.appendChild(document.createTextNode(''));
+      styleElement.setAttribute('react-fake-content', '');
+
+      document.head.appendChild(styleElement);
+
+      const index = styleElement.sheet.insertRule(
+        rule,
+        styleElement.sheet.cssRules.length
+      );
+
+      return function() {
+        styleElement.sheet.deleteRule(index);
+      };
+    } catch (err) {
+      console.error(
+        'An error happened on react-fake-content by trying to add stylesheet.'
+      );
+      console.error(rule);
+      console.error(err);
+    }
+  }, []);
+}
+
+export function Rectangle({
+  width = '100%',
+  height = '15px',
+  animation = true,
+  animationDuration = 1000,
+  animationName = 'reactFakeContentAnimation',
+  primaryColor = '#efefef',
+  secondaryColor = '#ddd',
+  style = {},
+  as = 'div',
+  ...props
+}) {
+  const keyframesRule = `
+      @keyframes ${animationName} {
+        from   { background-position: 0 center; }
+        to { background-position: -200% center; }
+      }
+      `;
+  useInjectStyle(keyframesRule);
+
+  const styles = {
+    display: 'block',
+    maxWidth: width,
+    height: height,
+    backgroundColor: primaryColor,
+    backgroundImage: `linear-gradient(to right, ${secondaryColor}, ${primaryColor}, ${secondaryColor})`,
+    backgroundSize: '200%',
+    borderRadius: '3px',
+    ...style
+  };
+
+  if (animation) {
+    styles.animation = `${animationName} ${animationDuration}ms linear infinite running`;
   }
+
+  return React.createElement(as, { ...props, style: styles });
+}
+
+export function Circle({ size = '60px', ...props }) {
+  return (
+    <Rectangle
+      {...props}
+      width={size}
+      height={size}
+      style={{
+        borderRadius: '50%',
+        ...props.style
+      }}
+    />
+  );
+}
+
+export function Line({ width = '100%', height = '15px', ...props }) {
+  return (
+    <Rectangle
+      {...props}
+      width={width}
+      height={height}
+      style={{
+        margin: '10px 0',
+        borderRadius: '5px',
+        ...props.style
+      }}
+    />
+  );
+}
+
+export function Square(props) {
+  return <Rectangle {...props} width={props.size} height={props.size} />;
 }
